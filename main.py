1264717ff63bc13bf50e545179b49cba611225bc
nalmt <nabil.lamrabet@pm.me>
2020-11-10 12:51:51 +0100

    add_agent()

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 # TP1 SMA 
 # 10/11/20
 
+import numpy as np
 from enum import Enum
 import random
-
-
-
-class Environment:
-
 
 n = 5
 matrix = np.full((n, n), '0')
 
 
+def add_agent(agent):
+    if matrix[agent.position[0]][agent.position[1]] == '0':
+        matrix[agent.position[0]][agent.position[1]] = agent.name
+    else:
+        print("Erreur ajout agent : agent deja present")
 
 class Agent:
 
@@ -22,9 +23,16 @@ class Agent:
         self.goal = goal
 
 
-
 def main():
-    environment = Environment()
+    a = Agent('A', (0, 1), (0, 2))
+    b = Agent('B', (1, 3), (0, 1))
+    c = Agent('C', (4, 1), (4, 1))
+    d = Agent('D', (2, 2), (4, 4))
+
+    for agent in (a, b, c, d):
+        add_agent(agent)
+
+    print(matrix)
 
 
 if __name__ == "__main__":
