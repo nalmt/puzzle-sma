c7d910f8d7d97ef7ba0b6c24658268cad384bb04
nalmt <nabil.lamrabet@pm.me>
2020-11-10 12:34:25 +0100

    structure

---
+++ main.py
@@ -0,0 +1,31 @@
+# TP1 SMA 
+# 10/11/20
+
+from enum import Enum
+import random
+
+
+
+class Environment:
+
+
+n = 5
+matrix = np.full((n, n), '0')
+
+
+
+class Agent:
+
+    def __init__(self, name, position, goal):
+        self.name = name
+        self.position = position
+        self.goal = goal
+
+
+
+def main():
+    environment = Environment()
+
+
+if __name__ == "__main__":
+    main()
